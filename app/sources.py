0a281e94d8ced6ddb9fd543eafbcf5f51225664f
Abdulrhmn bamsoud <abdaulrhamn7i@gmail.com>
2026-01-09 18:25:01 +0300

    Create sources.py

---
+++ app/sources.py
@@ -0,0 +1,24 @@
+# app/sources.py
+
+DEFAULT_SOURCES = [
+    "http://rss.cnn.com/rss/cnn_world.rss",
+    "http://feeds.bbci.co.uk/news/world/rss.xml",
+    "http://feeds.nbcnews.com/feeds/worldnews",
+    "http://feeds.abcnews.com/abcnews/internationalheadlines",
+    "https://www.cbsnews.com/latest/rss/world",
+    "https://rss.dw.com/rdf/rss-en-world",
+    "https://www.france24.com/en/rss",
+    "https://www.lemonde.fr/rss/une.xml",
+    "https://www.theguardian.com/world/rss",
+    "http://feeds.washingtonpost.com/rss/world",
+    "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+    "https://www.economist.com/international/rss.xml",
+    "https://www.foreignaffairs.com/rss.xml",
+    "https://thehill.com/feed/",
+    "https://www.axios.com/feeds/feed.rss",
+    "https://www.newsweek.com/rss",
+    "https://www.businessinsider.com/rss",
+    "https://feeds.a.dj.com/rss/RSSWorldNews.xml",  # WSJ: غالبًا paywall
+    "https://www.bloomberg.com/feed/podcast/etf-report.xml",  # Bloomberg: غالبًا paywall
+    "https://www.ft.com/world/rss",  # FT: sometimes 404
+]
